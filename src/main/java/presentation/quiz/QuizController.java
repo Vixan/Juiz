9c440ff1f5169f6cff8ea26f4a5b1904db66add6
Vixan <vixan.one@gmail.com>
2018-12-25 02:31:57 +0200

    :lipstick: Add quiz view and make some minor changes to dashboard view

---
+++ src/main/java/presentation/quiz/QuizController.java
@@ -0,0 +1,31 @@
+package presentation.quiz;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
+import shared.domain.Difficulty;
+import shared.domain.Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizController {
+    public ProgressBar timeProgressBar;
+    public VBox questionsContainer;
+    private Quiz startedQuiz;
+    private Difficulty selectedDifficulty;
+    private List<CheckBox> answerCheckboxes;
+
+    @FXML
+    public void initialize() {
+    }
+
+    public void setQuiz(Quiz quiz) {
+        startedQuiz = quiz;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        selectedDifficulty = difficulty;
+    }
+}
